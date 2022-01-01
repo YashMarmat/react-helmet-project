6e5ee880831966543226db934112e33a321dde7d
Yash <yash.nirrogyan@gmail.com>
2022-01-01 19:02:24 +0530

    dynamic-meta-tags try6

--- src/App.js
+++ src/App.js
@@ -4,28 +4,30 @@ import About from './components/About'
 import PageNotFound from './components/PageNotFound'
 import MyNavbar from './components/MyNavbar'
 import Contact from './components/Contact'
-import {Helmet} from "react-helmet";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter, Route, Routes } from 'react-router-dom' // NOTE: Switch is replace with Routes in react-router-dom v6
 
 const App = () => {
 
   return (
-    <div>
-      <BrowserRouter>
-        <MyNavbar />
-        <Helmet>
-          <title>Master Page</title>
-          <meta charSet="utf-8" />
-          <meta name="description" content="A React.js application" />
-        </Helmet>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <HelmetProvider>
+      <div>
+        <BrowserRouter>
+          <MyNavbar />
+          <Helmet>
+            <title>Master Page</title>
+            <meta charSet="utf-8" />
+            <meta name="description" content="A React.js application" />
+          </Helmet>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </HelmetProvider>
   )
 }
 
