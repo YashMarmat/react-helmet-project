fda125509077a42c5b68ca2b5128cc36a7d5a978
Yash <yash.nirrogyan@gmail.com>
2022-01-02 19:09:38 +0530

    testing new url

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,6 @@ const About = () => {
                 <meta charSet="utf-8" />
                 <title>About Page</title>
                 <meta name="description" content="about page description here" />
-                <link rel="canonical" href="https://branch2.d3tq5n7dq9hum4.amplifyapp.com/about" />
             </Helmet>
 
             <h1>
